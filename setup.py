b8825acd24d85b02379fd76668db2108853fbe32
cyprienmouton <cyprien.mouton@edu.devinci.fr>
2025-01-13 16:38:50 +0100

    Ajout du fichier setup.py pour la configuration du projet

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 16:23:41 2025
+
+@author: cypri
+"""
+
+from setuptools import setup
+
+setup(
+    name="game_of_life",
+    version="1.0",
+    author="Cyprien Mouton",
+    license="MIT",
+    long_description=open("README.md").read(),
+)
